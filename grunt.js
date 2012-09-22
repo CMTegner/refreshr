e70f42e4bdd3961b52f3d3b7965910183dedd463
Christian Tegnér <christian.tegner@gmail.com>
2012-09-22 16:48:07 +0200

    Adding grunt-vows config

--- grunt.js
+++ grunt.js
@@ -78,6 +78,9 @@ module.exports = function (grunt) {
                 worker: false,
                 wsh: false
             }
+        },
+        vows: {
+            reporter: "spec"
         }
     });
 
